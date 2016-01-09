ac1534eb67d4582d9c27acfe40192f043f1419d0
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-01-09 15:59:47 -0500

    Added code to add_to_products method to check for a duplicate title

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,24 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options = {})
-    @@products.push(self)
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    @@products.each do |product|
+      raise DuplicateProductError, "#{self.title}' already exists." if product.title == self.title
+    end
+    @@products.push(self)
   end
 
 end
