c79ef0ada47e4beb855b673be968c4a99e260b90
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-01-10 10:24:17 -0500

    Added methods to the Customers class to initialize instance, show a count of customers, and find a customer by name.

---
+++ lib/customer.rb
@@ -0,0 +1,29 @@
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize(options = {})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(name)
+    @@customers.each do |customer|
+      return customer if customer.name == name
+    end
+  end
+
+  private
+
+  def add_to_customers
+    @@customers.each do |customer|
+      raise DuplicateCustomerError, "'#{customer.name}' already exists" if customer.name == self.name
+    end
+    @@customers.push(self)
+  end
+end
